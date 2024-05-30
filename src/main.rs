21b62cb7f2d00c20003f80ed04966af9ab9775b7
Bao Ngo <nmtb23@gmail.com>
2024-05-30 09:55:13 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{io::{self, Write}, process::exit};
+use std::{
+    io::{self, Write},
+    process::exit,
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -13,16 +16,13 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
         let input = input.trim();
-        
-        let tokens = split_command(input);
-        match tokens[..] {
-            ["exit", exit_code] => exit(exit_code.parse::<i32>().unwrap()),
-            _ => println!("{}: command not found", input.trim())
+
+        let args = input.split_whitespace().collect::<Vec<&str>>();
+
+        match args[0] {
+            "exit" => exit(args[1].parse::<i32>().unwrap()),
+            "echo" => println!("{}", args[1..].join(" ")),
+            _ => println!("{}: command not found", input),
         }
-
     }
-}
-
-fn split_command(input: &str) -> Vec<&str> {
-    input.split(' ').collect()
 }
